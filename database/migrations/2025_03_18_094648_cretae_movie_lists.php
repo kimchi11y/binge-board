2af94a8cb3dea44fae7e5ba5e1137c69b9d9b2a6
hakim <abdulhakim0401@gmail.com>
2025-03-18 17:58:29 +0800

    Added models, migration

---
+++ database/migrations/2025_03_18_094648_cretae_movie_lists.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movie_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->integer('year');
+            $table->decimal('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movie_lists');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Movie;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,29 +9,7 @@ Route::get('/', function () {
 
 Route::get('/watched', function () {
     return view('watched', [
-        "movies" => [
-            [
-                'id' => 1,
-                'title' => 'The Shawshank Redemption',
-                'genre' => 'Drama',
-                'year' => 1994,
-                'rating' => 9.3
-            ],
-            [
-                'id' => 2,
-                'title' => 'The Godfather',
-                'genre' => 'Crime',
-                'year' => 1972,
-                'rating' => 9.2
-            ],
-            [
-                'id' => 3,
-                'title' => 'The Dark Knight',
-                'genre' => 'Action',
-                'year' => 2008,
-                'rating' => 9.0
-            ]
-        ]
+      "movies" => Movie::all()
     ]);
 });
 
