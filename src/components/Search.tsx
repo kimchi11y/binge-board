547b7d9d38848cbef625e5b849ff3f770783b511
hakim <abdulhakim0401@gmail.com>
2025-07-16 23:47:05 +0800

    Added handleSearch function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,9 @@
 function Search() {
+ 
+    function handleSearch() {
+        alert("Movie or TV show search initiated!");
+    }
 
-    
   return (
     <div className="flex justify-center ">
       
@@ -9,7 +12,7 @@ function Search() {
         placeholder="Search for movies or TV shows..."
         className="text-sm p-2 border border-gray-300 rounded md:w-1/2 lg:w-1/3"
       />
-      <button className=" p-2 ml-2 rounded-full shadow hover:bg-blue-200">
+      <button onClick={handleSearch} className=" p-2 ml-2 rounded-full shadow hover:bg-blue-200">
         🔍
       </button>
     </div>
